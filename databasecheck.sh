2ca80828dd49474094312ffc7475a0cb0105212a
echoe <sfelshman@gmail.com>
2014-01-22 13:35:50 -0600

    Cleaning up the bottom half of the script.

--- databasecheck.sh
+++ databasecheck.sh
@@ -36,21 +36,12 @@ do
      then
          echo "$table in $database is MyISAM, repairing with mysqlcheck :D"
          mysqlcheck --auto-repair --optimize $database $table 
-     elif [ $tabletype == "MEMORY" ]
-     then
-         echo "$table in $database is MEMORY and is in memory, we cannae help :| it's fast though :0 and deletes itself on any restart so if you're having any issueswith it you could do that I guess, or dump/recreate"
      elif [ $tabletype == "MERGE" ]
      then
          echo "$table in $database is MERGE, checking table :3 if you need to fix it might wanna run a UNION command? That's totes beyond me though, I just check tables and stuff."
          mysql -e "use $database; CHECK TABLE $table"
-     elif [ $tabletype == "FEDERATED" ]
-     then
-         echo "This table is hosted off-server. /whoa/ D: zomg. May wanna fix it over there but I don't check that noise"
-     elif [ $tabletype == "BDB" ] || [ $tabletype == "EXAMPLE" ] || [ $tabletype == "ARCHIVE" ] || [ $tabletype == "PERFORMANCE_SCHEMA" ] || [ $tabletype == "CSV" ] || [ $tabletype == "BLACKHOLE" ]
-     then
-         echo "$table in $database is something old or static, oh. Not altering @.@ if you wanna mess with this, do it on your own damn time."
      else
-         echo "$table in $database is not anything I recognize. :? Not in http://dev.mysql.com/doc/refman/5.0/en/storage-engines.html . Not altering."
+         echo "$table in $database has tabletype $tabletype and is not anything I actively fix. :? Not in http://dev.mysql.com/doc/refman/5.0/en/storage-engines.html . Not altering."
      fi
   done
 done
