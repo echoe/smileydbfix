f76be8cafab9c4b9c22b85beccf12b4d284f96cc
echoe <sfelshman@gmail.com>
2014-02-02 13:43:09 -0600

    Update databasecheck.sh

--- databasecheck.sh
+++ databasecheck.sh
@@ -1,7 +1,9 @@
 #MySQL database checker and fixer
-#Version 0.19
-#To parse logs: :D means it is repairing successfully. :| means did nothing. 
+#Version 0.20
+#To parse logs: :D means it is repairing successfully. :| means that it did nothing. 
 
+#this is so that you can use the current date in anything you need it in [logfiles,backups]
+thedate=`date | sed -e s/" "/_/g`;
 #Check to make sure mysql version is 5.x
 if [ `mysql -V | awk '{print $5}' | cut -d "." -f -1` == "5" ]
   then
@@ -18,22 +20,21 @@ fi
 echo -e "Would you like to make backups? y for yes"
 read backups
 if [ $backups == "y" ]; then
-  thedate=`date | sed -e s/" "/_/g`;
   mkdir -p /home/sqldumps/$thedate; 
   cd /home/sqldumps/$thedate; 
   for i in $(mysql -BNe 'show databases'| grep -v _schema); do `mysqldump $i > ./$i.sql` ; echo "we have backed up "$i | $log ;done
 fi
-echo -e "Would you like to take a logfile?"
-read logs
+#this functionality is broken. i am working on it
+#also, before this delete the old log file if it's already there, or move it, e.g.:
+#if [ -a (filename) ]; then mv (filename) (filename.thedate
 if [ $logs == "y" ]; then
-  log=">> /root/databasechecklog.txt"
-  else log=""
+  echo "sorry, this is disabled for now :/ please wait for me to fix this, or you can do it yourself. For now this is ato-writing to /root/databasechecklog.txt"
 fi
 echo "Actually running MyISAM check? y for yes"
 read myisam
 echo "Actually running InnoDB alter table? y for yes"
 read innodb
-echo "MyISAM: $myisam and InnoDB: $innodb" >> /root/databasechecklog.txt
+#echo the choices into the logfile when the logfile works
 #for loop, grab all the databases
 for database in $(mysql -e "SHOW DATABASES;"|tail -n+2); do
 #for each database, grab all the tables
@@ -43,20 +44,20 @@ for database in $(mysql -e "SHOW DATABASES;"|tail -n+2); do
 #check to see which table and fix it with a preferred method for each, or spout information
     if [ $tabletype == "InnoDB" ]; then
          if [ $innodb == y ]; then
-              echo "$table in $database is InnoDB, rebuilding with alter table command :D" $log
-              mysql -e "use $database; ALTER TABLE $table ENGINE = InnoDB" $log
-              else echo "$table in $database is InnoDB, doing nothing - disabled :|" $log
+              echo "$table in $database is InnoDB, rebuilding with alter table command :D"
+              mysql -e "use $database; ALTER TABLE $table ENGINE = InnoDB"
+              else echo "$table in $database is InnoDB, doing nothing - disabled :|"
          fi
     elif [ $tabletype == "MyISAM" ]; then
          if [ $myisam == y ]; then
-              echo "$table in $database is MyISAM, repairing with mysqlcheck :D" $log
-              mysqlcheck --auto-repair --optimize $database $table $log
-              else echo "$table in $database is MyIsam, doing nothing - disabled :|" $log
+              echo "$table in $database is MyISAM, repairing with mysqlcheck :D"
+              mysqlcheck --auto-repair --optimize $database $table
+              else echo "$table in $database is MyIsam, doing nothing - disabled :|"
          fi 
     elif [ $tabletype == "MERGE" ]; then
-         echo "$table in $database is MERGE, checking table :D if you need to fix it might wanna run a UNION command? That's totes beyond me though, I just check tables and stuff." $log
-         mysql -e "use $database; CHECK TABLE $table" $log
-    else echo "$table in $database has tabletype $tabletype and is not anything I actively fix. :? Not altering." $log
+         echo "$table in $database is MERGE, checking table :D Maybe you can fix with a UNION command, but I don't do that."
+         mysql -e "use $database; CHECK TABLE $table"
+    else echo "$table in $database has tabletype $tabletype and is not anything I actively fix. :? Not altering."
     fi
   done
 done
