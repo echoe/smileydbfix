f454910fece55309f8fcf381782bd17ff4e337f4
echoe <sfelshman@gmail.com>
2014-02-02 14:42:41 -0600

    Update databasecheck.sh
    Another logging attempt.

--- databasecheck.sh
+++ databasecheck.sh
@@ -1,42 +1,36 @@
-#MySQL database checker and fixer
-#Version 0.20
+#MySQL database checker and fixer https://github.com/echoe/smileydbfix/raw/master/databasecheck.sh
+#Version 0.21
 #To parse logs: :D means it is repairing successfully. :| means that it did nothing. 
 
 #this is so that you can use the current date in anything you need it in [logfiles,backups]
 thedate=`date | sed -e s/" "/_/g`;
-#Check to make sure mysql version is 5.x
-if [ `mysql -V | awk '{print $5}' | cut -d "." -f -1` == "5" ]
-  then
-  echo "You have MySQL 5 or an equivalent :D" | $log
-  else echo "you don't have MySQL 5! don't run this >.>" | $log; break
+#Tell them about the logs!
+echo "Logs for this are created in /tmp/dblogfile."
+#Check to make sure mysql version is 5.x . Also, reset the logs [tee, not tee -a]
+if [ `mysql -V | awk '{print $5}' | cut -d "." -f -1` == "5" ]; then
+  echo "You have MySQL 5 or an equivalent :D" tee /tmp/dblogfile
+  else echo "you don't have MySQL 5! don't run this >.>" | tee /tmp/dblogfile; break
 fi
-#ask because this takes forever D:
+#Ask because this takes forever D:
 echo -e "Would you like to check space for backups? y for yes"
 read checkspace
 if [ $checkspace == "y" ]; then
   datadir=`grep datadir /etc/my.cnf | sed s/"datadir="//g`
-  echo "Space left is:" `df -h | awk '{print $4}' | head -n2 | tail -n1` "and space MySQL takes up is:" `du -sh $datadir` | $log
+  echo "Space left is:" `df -h | awk '{print $4}' | head -n2 | tail -n1` "and space MySQL takes up is:" `du -sh $datadir` | tee -a /tmp/dblogfile
 fi
 echo -e "Would you like to make backups? y for yes"
 read backups
 if [ $backups == "y" ]; then
   mkdir -p /home/sqldumps/$thedate; 
   cd /home/sqldumps/$thedate; 
-  for i in $(mysql -BNe 'show databases'| grep -v _schema); do `mysqldump $i > ./$i.sql` ; echo "we have backed up "$i | $log ;done
+  for i in $(mysql -BNe 'show databases'| grep -v _schema); do `mysqldump $i > ./$i.sql` ; echo "we have backed up "$i | tee -a /tmp/dblogfile ;done
 fi
-#this functionality is broken. i am working on it
-#also, before this delete the old log file if it's already there, or move it, e.g.:
-#if [ -a (filename) ]; then mv (filename) (filename.thedate
-#echo -e "Would you like to take a logfile?"
-#read logs;
-#if [ $logs == "y" ]; then
-#  echo "sorry, this is disabled for now :/ please wait for me to fix this, or you can do it yourself. For now this is ato-writing to /root/databasechecklog.txt"
-#fi
 echo "Actually running MyISAM check? y for yes"
 read myisam
 echo "Actually running InnoDB alter table? y for yes"
 read innodb
 #echo the choices into the logfile when the logfile works
+echo "Backups=" $backups, "MyISAM=" $myisam, "InnoDB=" $innodb | tee -a /tmp/dblogfile
 #for loop, grab all the databases
 for database in $(mysql -e "SHOW DATABASES;"|tail -n+2); do
 #for each database, grab all the tables
@@ -46,20 +40,20 @@ for database in $(mysql -e "SHOW DATABASES;"|tail -n+2); do
 #check to see which table and fix it with a preferred method for each, or spout information
     if [ $tabletype == "InnoDB" ]; then
          if [ $innodb == y ]; then
-              echo "$table in $database is InnoDB, rebuilding with alter table command :D"
-              mysql -e "use $database; ALTER TABLE $table ENGINE = InnoDB"
-              else echo "$table in $database is InnoDB, doing nothing - disabled :|"
+              echo "$table in $database is InnoDB, rebuilding with alter table command :D" | tee -a /tmp/dblogfile
+              mysql -e "use $database; ALTER TABLE $table ENGINE = InnoDB" | tee -a /tmp/dblogfile
+              else echo "$table in $database is InnoDB, doing nothing - disabled :|" | tee -a /tmp/dblogfile
          fi
     elif [ $tabletype == "MyISAM" ]; then
          if [ $myisam == y ]; then
-              echo "$table in $database is MyISAM, repairing with mysqlcheck :D"
-              mysqlcheck --auto-repair --optimize $database $table
-              else echo "$table in $database is MyIsam, doing nothing - disabled :|"
+              echo "$table in $database is MyISAM, repairing with mysqlcheck :D" | tee -a /tmp/dblogfile
+              mysqlcheck --auto-repair --optimize $database $table | tee -a /tmp/dblogfile
+              else echo "$table in $database is MyIsam, doing nothing - disabled :|" | tee -a /tmp/dblogfile
          fi 
     elif [ $tabletype == "MERGE" ]; then
-         echo "$table in $database is MERGE, checking table :D Maybe you can fix with a UNION command, but I don't do that."
-         mysql -e "use $database; CHECK TABLE $table"
-    else echo "$table in $database has tabletype $tabletype and is not anything I actively fix. :? Not altering."
+         echo "$table in $database is MERGE, checking table :D Maybe you can fix with a UNION command, but I don't do that." | tee -a /tmp/dblogfile
+         mysql -e "use $database; CHECK TABLE $table" | tee -a /tmp/dblogfile
+    else echo "$table in $database has tabletype $tabletype and is not anything I actively fix. :? Not altering." | tee -a /tmp/dblogfile
     fi
   done
 done
